92d4ad7e68a226fbc70cfc466c6be55b887d80f5
SimonO2001 <Oksen.04@hotmail.com>
2023-09-28 15:24:55 +0200

    updated_styling

--- upload_csv/urls.py
+++ upload_csv/urls.py
@@ -9,7 +9,7 @@ from uploader import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload/', UploadView.as_view(), name='upload_data'),  # Update the name here
-    path('display/', views.display_data, name='display_data'),
+    path('', views.display_data, name='display_data'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('add/', views.add_product, name='add_product'),
