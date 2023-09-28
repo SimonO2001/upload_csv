215793bef49f9bf7fd8ac4cc5d7a546277d2e49d
SimonO2001 <Oksen.04@hotmail.com>
2023-09-28 08:54:18 +0200

    Updated_styling

--- upload_csv/urls.py
+++ upload_csv/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('add/', views.add_product, name='add_product'),
+    path('upload/', UploadView.as_view(), name='upload_data'),
 ]
