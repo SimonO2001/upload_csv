14a468c35650d7d1bef6d2a70412db56fc551489
SimonO2001 <Oksen.04@hotmail.com>
2023-10-12 12:34:11 +0200

    updated

--- upload_csv/settings.py
+++ upload_csv/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%zew7fa7n4lr&$-19=wiyq9$8g@&ctwxc!#pwwjb=h$t5bt2=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['195.201.89.91']
 
 
 
